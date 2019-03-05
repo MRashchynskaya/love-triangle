68fd83b486526955e4f857f618d5f9f71371c2eb
Margarita Rashchynskaya <m.rashchynskaya@invento.by>
2019-03-05 16:38:20 +0300

    feat: count number of love triangles

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let counter = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    let personFirst = preferences[i] - 1;
+    let personSecond = preferences[personFirst] - 1;
+    let personThird = preferences[personSecond] - 1;
+    if (personThird === i) {
+      counter++;
+    };
+  }
+  return Math.floor(counter / 3);
 };
